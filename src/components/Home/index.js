759e546c67ef1ecf7d85060edf1fb034f5184d44
Samueljoli <samuel.joli.ftn@gmail.com>
2018-10-23 09:24:07 -0400

    Update animations and add white bg-color

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,12 @@
-
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Row, Col, Button } from 'antd';
+import { Fade } from 'react-reveal';
 import media from '../../helpers/media';
-
-const Container = styled.div`
-  height: 400px;
-  width: 80%;
-`;
 
 const StyledH1 = styled.h1`
   margin-bottom: 0;
+  font-size: 3em;
 `;
 
 const StyledH4 = styled.h4`
@@ -26,39 +22,40 @@ const StyledCol = styled(Col)`
   ${media.sm`
     height: 100vh;
     text-align: left;
-    padding: 80px;
+    padding: 100px;
   `}
 `;
 
 class LandingPage extends Component {
   render() {
-    const style1 = {
-      backgroundColor: 'red',
-      height: 400,
+    const colBaseStyle = {
+      lg: 12,
+      sm: 24,
     };
-    const style2 = {
-      backgroundColor: 'blue',
-      height: 400,
-    };
+
     return (
-      <Row> 
-        <StyledCol lg={12} sm={24} style={{ marginTop: 64 }}>
-          <StyledH1>Investing.</StyledH1>
-          <StyledH1>Now for the rest of us.</StyledH1>
-          <div>
-            <StyledH4>Robinhood lets you invest in the stock market for free, directly from your phone or desktop.</StyledH4>
-          </div>
-          <Button type="primary">
-            Sign Up
-          </Button>
+      <Row style={{ backgroundColor: 'white' }}>
+        <StyledCol {...colBaseStyle} style={{ marginTop: 64 }}>
+          <Fade delay={500}>
+            <StyledH1>Investing.</StyledH1>
+            <StyledH1>Now for the rest of us.</StyledH1>
+            <div>
+              <StyledH4>Robinhood lets you invest in the stock market for free, directly from your phone or desktop.</StyledH4>
+            </div>
+          </Fade>
+          <Fade delay={1000}>
+            <Button type="primary">
+              Sign Up
+            </Button>
+          </Fade>
         </StyledCol>
-        <StyledCol lg={12} sm={24} style={{ marginTop: 64 }}>
+        <StyledCol {...colBaseStyle} style={{ marginTop: 64 }}>
           <h1> Two </h1>
         </StyledCol>
-        <StyledCol lg={12} sm={24}>
+        <StyledCol {...colBaseStyle}>
           <StyledH1> Three </StyledH1>
         </StyledCol>
-        <StyledCol lg={12} sm={24}>
+        <StyledCol {...colBaseStyle}>
           <h1> Four </h1>
         </StyledCol>
       </Row>
