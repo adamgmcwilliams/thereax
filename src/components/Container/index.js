40e0bcd05e9c64b545ba8fa88e83aac7713814ef
Samuel Joli <sjoli@xogrp.com>
2018-10-30 09:26:19 -0400

    Add Logo to header

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import { Menu, Layout } from 'antd';
+import styled from 'styled-components';
 import history from '../../helpers/history';
+import media from '../../helpers/media';
 
 const { Header, Content } = Layout;
+const LogoContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 20px;
+  ${media.sm`
+    position: absolute;
+    top: 0;
+    left: 30px;
+  `}
+`;
 
 class Container extends Component {
   render() {
     return (
       <Layout>
-      <Header style={{ position: 'fixed', zIndex: 1, width: '100%', padding: 0 }}>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%', padding: 0, textAlign: 'right' }}>
         <Menu
           theme="light"
           mode="horizontal"
-          defaultSelectedKeys={['2']}
+          defaultSelectedKeys={['home']}
           style={{ lineHeight: '64px' }}
         >
+          <LogoContainer>
+            <h1> R | E | A | X </h1>
+          </LogoContainer>
           <Menu.Item
             key="home"
             onClick={() => {
