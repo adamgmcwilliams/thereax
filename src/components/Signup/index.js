41a957064e6019c2449611c58c57e107fb7618f1
Samueljoli <samuel.joli.ftn@gmail.com>
2018-11-01 22:53:57 -0400

    Fix sign up mobile typed assist

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 import { Fade } from 'react-reveal';
 import styled from 'styled-components';
+import media from '../../helpers/media';
 
 import Form from './Form';
 import TypedAssist from './TypedAssist';
@@ -10,14 +11,25 @@ const colBaseStyle = {
   lg: 12,
   sm: 24,
   xs: 24,
-  style: {
-    marginTop: 64
-  }
 };
 
 const Container = styled.div`
   padding: 35px;
-  font-size: 2em;
+  padding-top: 0px;
+  font-size: 1.5em;
+  text-align: center;
+  ${media.sm`
+    padding: 35px;
+    font-size: 2em;
+    text-align: left;
+  `}
+`;
+
+const StyledCol = styled(Col)`
+  margin-top: ${props => props.marginTop ? props.marginTop : '64px'}
+  ${media.sm`
+    margin-top: 64px;
+  `}
 `;
 
 const typedStrings = [
@@ -35,14 +47,14 @@ class Signup extends Component {
         align="middle"
         style={{ backgroundColor: 'white' }}
       >
-        <Col
+        <StyledCol
           {...colBaseStyle}
         >
           <Fade bottom delay={100}>
             <Form />
           </Fade>
-        </Col>
-        <Col {...colBaseStyle}>
+        </StyledCol>
+        <StyledCol {...colBaseStyle} marginTop="0px">
           <Fade delay={1000}>
             <Container>
               <TypedAssist
@@ -51,7 +63,7 @@ class Signup extends Component {
               />
             </Container>
           </Fade>
-        </Col>
+        </StyledCol>
       </Row>
     );
   }
