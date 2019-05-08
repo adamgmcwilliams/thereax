ca58ccd19d5b743313fa786fac790eba266d6883
sjoli <sjoli@xogrp.com>
2019-05-08 17:47:18 -0400

    Revert "Press release changes"
    This reverts commit e43f7189f130435640e814110497493dafb0d075.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -60,6 +60,18 @@ class Container extends Component {
               <h1> R | E | A | X </h1>
             </LogoContainer>
             <Menu.Item
+              key="home"
+              onClick={() => {
+                history.push('/');
+
+                this.setState({
+                  selectedKeys: ['home'],
+                });
+              }}
+            >
+              Home
+            </Menu.Item>
+            <Menu.Item
               key="signup"
               onClick={() => {
                 history.push('/signup');
