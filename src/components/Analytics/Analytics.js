b4c12dc7a63798d643771021484b95af5d8a4739
ali zain <alizain721@gmail.com>
2019-10-31 13:59:09 +0500

    Analytics: Migrate to styled components based styling

---
+++ src/components/Analytics/Analytics.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const AnalyticsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+  margin-top: 4%;
+  background-color: #ffffff;
+  width: 100%;
+  align-items: center;
+`;
