cfb5e331617ec68fb9eee1c439a0b626d5f833d7
ali zain <alizain721@gmail.com>
2019-11-06 16:43:45 +0500

    Store: Configure redux dev tools

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,9 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from '../reducers';
 import { initialState } from './state';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default () => {
-  return createStore(reducers, initialState, applyMiddleware(thunk));
+  return createStore(reducers, initialState, composeEnhancer(applyMiddleware(thunk)));
 }
