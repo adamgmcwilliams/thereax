c4a39539b9ce1ce0191d6f63da9af57d00b5ca66
ali zain <alizain721@gmail.com>
2019-11-08 15:37:14 +0500

    Search: Implement drop down based search on districts

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
-import { Input } from 'antd';
+import { AutoComplete  } from 'antd';
+import { connect } from 'react-redux';
 
 import { SearchBoxContainer } from './Search.js';
+import { setCurrentMarket } from '../../redux/actions';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    let { stateData } = props;
+    this.state = {stateData: stateData, states: [] }
+  }
+
   render() {
     return(
       <SearchBoxContainer >
-        <Input placeholder="Search Districts" maxLength={5000} width={100} />
+        <AutoComplete placeholder="Search Districts" style={{ width: 400 }} dataSource={this.state.states}
+          onSelect={this.renderMarket} />
       </ SearchBoxContainer>
     );
   }
+
+  renderMarket = (value, option) => {
+    let currentMarket = this.state.stateData.markets.filter((market) => {
+      return Object.keys(market)[0].toUpperCase() === value
+    })
+    this.props.setCurrentMarket(currentMarket[0])
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    let states = nextProps.stateData.markets.map((market) => Object.keys(market)[0].toUpperCase())
+    return {
+      stateData: nextProps.stateData,
+      states: states
+    }
+  }
 }
 
-export default Search;
+const mapStateToProps = (state) => {
+  return { stateData: state.stateData }
+}
+
+export default connect(mapStateToProps, { setCurrentMarket })(Search);
