0a4ab2235fc4c5ecc014060a8aa99a50ff34e046
ali zain <alizain721@gmail.com>
2019-11-11 17:23:49 +0500

    Comment: Refactor code

--- src/components/Analytics/CommentList/Comment/Comment.jsx
+++ src/components/Analytics/CommentList/Comment/Comment.jsx
@@ -24,50 +24,31 @@ class Comment extends React.Component {
     this.state = { commentText: comment.comment, commentTime: comment.timestamp, commentUserPhoto: comment.userPhoto, likes: comment.likes,
       dislikes: comment.dislikes }
   }
+
   render() {
     return(
       <CommentContainer>
         <CommentUser>
-          <Avatar size={30} src={this.getCommentUserPhoto()} />
+          <Avatar size={30} src={this.state.commentUserPhoto} />
         </CommentUser>
         <CommentContentContainer>
-          <CommentText> {this.getCommentText()} </CommentText>
+          <CommentText> {this.state.commentText} </CommentText>
           <CommentMetaDataContainer>
-            <ComentTime> <Moment parse="YYYY-MM-DD HH:mm" fromNow> {this.getCommentTime()} ago</Moment> </ComentTime>
+            <ComentTime> <Moment parse="YYYY-MM-DD HH:mm" fromNow> {this.state.commentTime} ago</Moment> </ComentTime>
             <CommentLikesAndDislikes>
               <CommentLikeContainer>
                 <FontAwesomeIcon icon={faThumbsUp} size="xs" />
-                <TotalLikesAndDislikes> {this.getLikes()} </TotalLikesAndDislikes>
+                <TotalLikesAndDislikes> {this.state.likes} </TotalLikesAndDislikes>
               </CommentLikeContainer>
               <CommentDislikeContainer>
                <FontAwesomeIcon icon={faThumbsDown} size="xs" />
-               <TotalLikesAndDislikes> {this.getDislikes()} </TotalLikesAndDislikes>
+               <TotalLikesAndDislikes> {this.state.dislikes} </TotalLikesAndDislikes>
               </CommentDislikeContainer>
             </CommentLikesAndDislikes>
           </CommentMetaDataContainer>
         </CommentContentContainer>
       </CommentContainer>
     )
-  }
-
-  getCommentText = () => {
-    return this.state.commentText;
-  }
-
-  getCommentTime = () => {
-    return this.state.commentTime;
-  }
-
-  getCommentUserPhoto = () => {
-    return this.state.commentUserPhoto;
-  }
-
-  getLikes = () => {
-    return this.state.likes;
-  }
-
-  getDislikes = () => {
-    return this.state.dislikes;
   }
 }
 
