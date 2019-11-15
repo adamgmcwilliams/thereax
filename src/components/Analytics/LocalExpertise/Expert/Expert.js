0e430ab1039853f51ed5d65d07f9e6d61ab0017f
ali zain <alizain721@gmail.com>
2019-11-15 12:58:31 +0500

    Expert: Implement a modal to contact expert

--- src/components/Analytics/LocalExpertise/Expert/Expert.js
+++ src/components/Analytics/LocalExpertise/Expert/Expert.js
@@ -24,3 +24,73 @@ export const ExpertName = styled.div`
 export const ExpertPhoneNumber = styled.div`
   font-size: 0.8em;
 `
+export const ExpertDetailModal = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+`
+export const ExpertAvatar = styled.div`
+  width: 30%;
+`
+export const ExpertData = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+`
+export const ExpertAbout = styled.div`
+  font-size: 0.85em;
+`
+export const ExpertContactContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #686868;
+`
+export const MessageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-around;
+`
+export const UserContactDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  justify-content: space-between;
+`
+export const UserOtherContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  justify-content: space-between;
+  height: 10vh;
+  align-items: flex-end;
+`
+export const ContacatLocalExpertHeading = styled.div`
+  display: flex;
+  margin-bottom: 2%;
+  font-weight: bold;
+`
+export const MessageSubmitButton = styled.div`
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  background-color: #98ff98;
+  width: 15%;
+  display: flex;
+  justify-content: center;
+  margin-left: 85%;
+  margin-top: 2%;
+  border-radius: 25px;
+`
+export const MessageModalContainer = styled.div`
+  border-radius: 50px;
+`
+export const MessageModalFooter = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  font-size: 0.6em;
+  margin-top: 5%;
+`
